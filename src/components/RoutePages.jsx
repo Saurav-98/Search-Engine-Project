3347177be77bd341f7ec0b2733feaa2f4fced831
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-04 19:18:56 +0530

    Routes Updated

--- src/components/RoutePages.jsx
+++ src/components/RoutePages.jsx
@@ -8,11 +8,10 @@ const RoutePages = () => {
     <div className="p-4">
       <Routes>
         <Route path="/" exact element={<Navigate to="/search" />} />
-        <Route
-          exact
-          path={['/search', '/images', '/news', '/videos']}
-          element={<Results />}
-        />
+        <Route exact path="/search" element={<Results />} />
+        <Route exact path="/images" element={<Results />} />
+        <Route exact path="/news" element={<Results />} />
+        <Route exact path="/videos" element={<Results />} />
       </Routes>
     </div>
   );
